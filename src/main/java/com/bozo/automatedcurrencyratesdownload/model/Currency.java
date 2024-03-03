bb68029ba4080bea439e612fc7a7b1b595ddabe8
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2024-03-03 19:13:52 +0100

    create currency model #2

---
+++ src/main/java/com/bozo/automatedcurrencyratesdownload/model/Currency.java
@@ -0,0 +1,28 @@
+package com.bozo.automatedcurrencyratesdownload.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class Currency {
+    private String currency;
+    private String code;
+    private BigDecimal mid;
+
+    public Currency(String currency, String code, double mid){
+        this(currency, code, BigDecimal.valueOf(mid));
+    }
+
+    public void setMid(double mid) {
+        this.mid = BigDecimal.valueOf(mid);
+    }
+
+    public void setMid(BigDecimal mid){
+        this.mid = mid;
+    }
+}
