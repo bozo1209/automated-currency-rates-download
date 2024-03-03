4b99b9f972e422e373537d6a4c6c08b969154252
Mateusz Gulczewski <55853886+bozo1209@users.noreply.github.com>
2024-03-03 18:23:09 +0100

    create project closes #1

---
+++ src/main/java/com/bozo/automatedcurrencyratesdownload/AutomatedCurrencyRatesDownloadApplication.java
@@ -0,0 +1,13 @@
+package com.bozo.automatedcurrencyratesdownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedCurrencyRatesDownloadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedCurrencyRatesDownloadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bozo/automatedcurrencyratesdownload/AutomatedCurrencyRatesDownloadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bozo.automatedcurrencyratesdownload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatedCurrencyRatesDownloadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
